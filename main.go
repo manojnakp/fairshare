bcd867eb89ebb29b2ad4c3740592daa17d269a37
Manojna <manojnakp2003@gmail.com>
2024-03-08 11:33:32 +0530

    feat: minimal HTTP server

--- main.go
+++ main.go
@@ -1,7 +1,26 @@
 package main
 
-import "fmt"
+import (
+	"flag"
+	"log"
+	"net"
+	"net/http"
+)
+
+var (
+	Port = "8080"
+	Host = ""
+)
+
+func init() {
+	flag.StringVar(&Host, "host", Host, "host to listen on")
+	flag.StringVar(&Port, "port", Port, "port to listen on")
+}
 
 func main() {
-	fmt.Println("Hello", "Fairshare")
+	flag.Parse()
+	addr := net.JoinHostPort(Host, Port)
+	log.Println("server listening on", addr)
+	err := http.ListenAndServe(addr, nil)
+	log.Fatal(err)
 }
