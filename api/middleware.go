5756785989fc70b9539e966a7f6c03b365238fce
Manojna <manojnakp2003@gmail.com>
2024-03-08 19:45:52 +0530

    feat: health checker HeartBeat middleware
    - replace HealthCheck route
    
    TODO:
    - [ ] simplify `api.With` in cli/main.go
    - [ ] API server logic should stay in `api` package.

--- api/middleware.go
+++ api/middleware.go
@@ -6,10 +6,14 @@ import (
 	"net/http"
 	"net/url"
 	"runtime/debug"
+	"strings"
 	"time"
 
 	"github.com/manojnakp/fairshare/internal"
 )
+
+// HealthChecker is the URI endpoint at which heart beat messages are served.
+var HealthChecker = "/health"
 
 // Middleware is an alias for HTTP middleware.
 type Middleware = func(http.Handler) http.Handler
@@ -24,7 +28,29 @@ func With(middlewares ...Middleware) Middleware {
 	}
 }
 
-// Logger is a HTTP Middleware to log handling information about every
+// HeartBeat is an HTTP Middleware that listens to heart beat (alive)
+// requests at the HealthChecker endpoint. Basically, it returns with a
+// `200 OK` message as long as the server is alive and kicking.
+func HeartBeat(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		if !strings.EqualFold(r.URL.Path, HealthChecker) {
+			/* path does not match */
+			next.ServeHTTP(w, r)
+			return
+		}
+		const MethodGet = "GET"
+		if r.Method != MethodGet {
+			/* method does not match */
+			AllowOnly{MethodGet}.ServeHTTP(w, r)
+			return
+		}
+		/* prevent caching */
+		w.Header().Set("Cache-Control", "no-cache, no-store")
+		PlainText(http.StatusOK).ServeHTTP(w, r)
+	})
+}
+
+// Logger is an HTTP Middleware to log handling information about every
 // incoming request.
 func Logger(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
--- api/router.go
+++ api/router.go
@@ -8,13 +8,5 @@ import (
 func Router() *http.ServeMux {
 	mux := http.NewServeMux()
 	mux.Handle("/", PlainText(http.StatusNotFound))
-	mux.HandleFunc("GET /health", HealthCheck)
-	mux.Handle("/health", AllowOnly{"GET"})
 	return mux
-}
-
-// HealthCheck handles the server health endpoint. Basically responds
-// `200 OK` as long as server is alive and kicking.
-func HealthCheck(w http.ResponseWriter, r *http.Request) {
-	PlainText(http.StatusOK).ServeHTTP(w, r)
 }
